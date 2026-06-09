d0effd6150402385a7183ec93dd7d5a3e09d60bf
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-06-09 22:32:50 +0530

    complete calculator

--- day-007/calculatorgrade.py
+++ day-007/calculatorgrade.py
@@ -25,3 +25,22 @@ def root(n):
     return pow(n ,1/2)
 rec()
 
+# GRADE CALCULATOR:
+
+score = float(input("what is your score in the range 0-100? "))
+
+
+def grade(score):
+    if score >=90:
+        print("A")
+    elif score >=80:
+        print("B")
+    elif score >=70:
+        print("c")
+    elif score >=60:
+        print("D")
+    else: print("F")
+
+
+grade(score)
+
