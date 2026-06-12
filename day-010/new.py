156cee3bff231fe2fff2982d4c8a5c5a47cc9ee5
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-06-12 20:54:20 +0530

    game

--- day-010/new.py
+++ day-010/new.py
@@ -3,21 +3,29 @@
 def hello(to="troublemaker!"):
     print("hello,", to)
 
-hello(to)
+hello()
 
-name = input("what is your name?").title().strip()
+name = input("what is your name? ").title().strip()
 
 hello(name)
 
 purpose = input("what brings you here today? fun or work?")
 
 if purpose == "fun":
-    print("cool! lets play a game.").sep="!"
+    print("cool! lets play a game.")
 else:
-    print("oops! wrong answer.").sep="!"
+    print("oops! wrong answer. choose again")
+    purpose = input("what brings you here today? fun or work?")
+    
 
 number = input("choose a number between 0-10")
- 
+if number >10:
+    print("troublemaker! choose a number between 0-10")
+elif number <0:
+    print("nah ah! troublemaker, choose a number between 0-10")
+elif number >=5:
+    print("alright!")
+
 
 
 
