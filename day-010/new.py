a0cbe136fc51d32b4060d037d17219bc0a37fe0e
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-06-12 20:33:53 +0530

    game try

---
+++ day-010/new.py
@@ -0,0 +1,25 @@
+#Day 10 - Aditi Sai Sharma - June 12, 2026
+
+def hello(to="troublemaker!"):
+    print("hello,", to)
+
+hello(to)
+
+name = input("what is your name?").title().strip()
+
+hello(name)
+
+purpose = input("what brings you here today? fun or work?")
+
+if purpose == "fun":
+    print("cool! lets play a game.").sep="!"
+else:
+    print("oops! wrong answer.").sep="!"
+
+number = input("choose a number between 0-10")
+ 
+
+
+
+
+
