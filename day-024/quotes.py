1526414b4f2049407eeb9b05a6e387cff627b486
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-06-26 23:38:07 +0530

    quotes

---
+++ day-024/quotes.py
@@ -0,0 +1,9 @@
+#Day 24 - Aditi Sai Sharma - June 26, 2026
+
+#quotes:
+#Create the thingd you wish existed.
+#Your future depends on what you do today.
+
+print("Create the thingd you wish existed.")
+print("Your future depends on what you do today.")
+print("")
