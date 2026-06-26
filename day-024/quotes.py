e0db0a55668c93af6e53bec501e47bdd243bdb43
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-06-26 23:50:27 +0530

    quotes

--- day-024/quotes.py
+++ day-024/quotes.py
@@ -4,9 +4,9 @@
 #Create the thingd you wish existed.
 #Your future depends on what you do today.
 
-print("Create the thingd you wish existed.")
+print("Create the things you wish existed.")
 print("Your future depends on what you do today.")
 print("Toleration will eventually lead to disgust.")
 print("dreams come true when you least expect it.")
 print("Feel the fear and do it anyway.")
-print("the more delusionalyy optimistic you are, the more life rewards you.")
+print("The more delusionalyy optimistic you are, the more life rewards you.")
