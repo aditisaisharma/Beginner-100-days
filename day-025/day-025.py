11e6c7b0e0af62f81f3611e2cd6dcc98ccb793e0
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-06-27 23:53:03 +0530

    day-025

--- day-025/day-025.py
+++ day-025/day-025.py
@@ -1,3 +1,15 @@
 #Day 25 - Aditi Sai Sharma - June 27, 2026
 
 print("I'm my own support system.")
+
+name = input("whats your name?")
+if name == "ambrose":
+    print("Adequately indeed my little ifrit")
+elif name == "lilly":
+    print("yes sir, ofcourse, sir!")
+elif name == "karim":
+    print("sahib, sahiba")
+elif name == "Carter":
+    print("au revoir miss linton")
+elif name == "dalgliesh":
+    ("die, dogleash.")
