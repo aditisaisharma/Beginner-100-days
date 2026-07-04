37f1b4f142b54171bf0bc242eb0e8c4bf8eaef7e
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-04 22:56:49 +0530

    day-32

--- day-032/day-032.py
+++ day-032/day-032.py
@@ -1,1 +1,57 @@
-#Day 32 - Aditi Sai Sharma - July 4, 2026
+#Day 32 - Aditi Sai Sharma - July 4, 2026ef main():
+    x = int(input("whats x?"))
+    if even(x):
+        print("even")
+    else:
+        print("odd")
+
+def even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+main()
+
+#calculator
+
+x = float(input('whats x? '))
+y = float(input('whats y? '))
+
+#addition:
+print(x+y)
+
+#subtraction:
+print(x-y)
+
+#multiplication:
+print(x*y)
+
+#division:
+print(x/y)
+
+#power:
+print(pow(x, y))
+
+#squareroot:
+print(pow(x, 1/2))
+
+#square:
+print(x*x)
+
+#odd/even
+if x % 2 ==0:
+    print("even")
+else:
+    print("odd")
+
+#>/<
+if x>5:
+    print('x is geater than 5')
+elif x<5:
+    print("x is less than 5")
+else:
+    print("x is equal to 5")
+
+#curious to program lcm/hcf and prime factors and trigonometry and probability
+
