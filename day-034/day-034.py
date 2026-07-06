6c38c54fc7169a243888ef9b02cb025cf283cc65
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-06 23:33:56 +0530

    day-034

--- day-034/day-034.py
+++ day-034/day-034.py
@@ -1,1 +1,12 @@
 #Day 34 - Aditi Sai Sharma - July 6, 2026
+
+print("im so grateful to be so successful at my young age. i have realised how i can work wonders and" \
+" do anything that i wish to.")
+
+for i in range(6):
+    print("I'm Aditi Sai Sharma.")
+
+def hello(to="world"):
+    print("hello,", to)
+
+hello()
