947e71a0101bb89c6db1839423f7f2575d482951
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-07 23:54:10 +0530

    day

---
+++ day-035/day-035.py
@@ -0,0 +1,110 @@
+#Day 35 - Aditi Sai Sharma - July 7, 2026
+"""
+INSTRUCTIONS FOR MAKING A PEANUT BUTTER SANDWICH:
+
+Buy medium sized sliced white bread and a bottle of peanut butter from a general store.
+Get a spoon and a plate.
+open the cap of P.B. bottle and take out two bread slices.
+scoop out 1 full spoon of butter and put on 1 bread.
+equally distribute the butter on the bread.
+put the other bread on top of the butter laden bread and apply pressure on top of the top-face of the 2 attached 
+breads.
+
+"""
+
+"""
+ROBOTIC VERSION:
+
+
+
+1.Ask for 500 indian rupees from the person who assigned you this task.
+2.Ask the commander for access to a local map.
+(also ask for a plastic bag)
+3.Persuade them into providing you with cash, bag and the map by saying - "for the sandwich to be made, i'd require
+to buy bread, a teaspoon, a plate and peanut butter from a general store with this money and without the cash or map 
+I can not continue the task.".
+
+[only move forward to the next step when the cash is given to you by the commander-of his own
+free will, or else, stop immediately and do not move to the next step].
+
+4.find a general store on the map - a shop that sells a wide variety of goods, 
+typically one in a small town or village.
+
+5. with the bag and cash, in right hand, walk to the store on foot by following general footpath
+6. avoid contact with any vehicle/ living being/ non living being, except roads.
+
+7.follow zebra crossing- A zebra crossing is a type of pedestrian crossing designed 
+to allow people to cross the road safely without the need for traffic lights. It is easily identifiable by its 
+alternating black and white stripes painted across the width of the road, resembling a zebra's coat, and often 
+accompanied by Belisha beacons—flashing orange lights on black and white posts at either end of the crossing, 
+
+8.stop on the path at green light, walk forward again at red light available on crossroads 
+(if any without zebra-crossing)
+
+9. reach the store.
+
+
+10. go inside the store and face the person behind a counter inside.
+11. greet the person with a smile.
+12. Ask for medium sized, packet of sliced white bread.
+13. ask for a small bottle of peanut butter.
+14. ask for a  teaspoon and a plate.
+
+(collect the aforementioned items in the bag)
+15. give the person the 500 rupee note and ask for change (leftover money).
+
+(if the store does not have any one of the item listed above: find another store and 
+follow all steps from 4 to 11 and 15 again, then ask for the left item. follow this routine untill you 
+have everything you need)
+
+16. with every item listed in bag and the cash returned return back to where you started from, following the same
+path backwards.
+17. return the leftover cash to the commander.
+18. ask for access to a straight polished surface where the aforementioned things can be kept.
+[only move forward to the next step when the table is given to you by the commander-of his own
+free will, or else, stop immediately and do not move to the next step].
+19. pull out the items from the bag one by one, in any order, and place them on the surface.
+20. keep the plate, the base of the outer curved part on the surface such that the plate has space
+ to be filled inside the curve, right in front of you.
+21. open the packet of bread then place it beside the plate, on right.
+22. take out 2 slices of bread and place it in the centre of the plate.
+23. open the cap of the bottle of peanut butter and place it on the left side of the plate.
+24. take the teaspoon and hover it over the peanut butter.
+25. put the teaspoon gently inside the butter bottle such that only the top curve of the spoon 
+is dipped inside the butter.
+26. gently scoop out the butter, about 3/4 of the spoon, from the bottle.
+27. wipe the excees butter at the bottom of the spoon, on the clean top-inside of the bottle.
+28. deposit the butter from the spoon on the centre of 1st bread, facing upwards.
+29. spread the butter evenly across the bread with the back of the spoon.
+30. keep the used spoon on the left side of the plate.
+31. put the 2nd bread on top of the butter laden part of the 1st bread, symmetrically.
+32. apply pressure on the top of the top-face of the bread sandwich to attach them.
+
+33. close the bottle of peanut butter.
+34. fold the top of the packet of bread.
+35. keep the peanut butter bottle and bread packet inside the bag.
+36. return the bag to the commander.
+
+37. the sandwich is ready. 
+give the sandwich, placed on top of the plate with the spoon on its side, to your commander.
+
+"""
+
+
+print("Persuade them into providing you with cash, bag and the map by saying - \"for the sandwich to be made, '\
+I would require to buy bread, a teaspoon, a plate and peanut butter from a general store with this money and '\
+without the cash or map-I can not continue the task.\"")
+
+print("only move forward to the next step when the cash is given to you by the commander-of his own '\
+free will, or else, stop immediately and do not move to the next step. ")
+
+print("follow zebra crossing- A zebra crossing is a type of pedestrian crossing designed  '\
+to allow people to cross the road safely without the need for traffic lights. It is easily identifiable by its '\
+alternating black and white stripes painted across the width of the road, resembling a zebra's coat, and often '\
+accompanied by Belisha beacons—flashing orange lights on black and white posts at either end of the crossing. ")
+
+print("if the store does not have any one of the item listed above: find another store and '\
+follow all steps from 4 to 11 and 15 again, then ask for the left item. follow this routine untill you '\
+have everything you need")
+
+print("give the sandwich, placed on top of the plate with the spoon on its side, to your commander")
