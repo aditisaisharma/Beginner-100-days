262ae323af27f4b4920924c4dfd612b8e041d1c9
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-08 23:24:09 +0530

    day-036

--- day-036/day-036.py
+++ day-036/day-036.py
@@ -1,1 +1,57 @@
 #Day 36 - Aditi Sai Sharma - July 8, 2026
+
+x = int(input("whats x? "))
+
+if x % 2 == 0:
+    print('even')
+else:
+    print("odd")
+
+x = int(input("whats x? "))
+
+
+        
+
+def even(n):
+    if x % 2 == 0:
+        print("even")
+    else:
+        print('odd')
+
+even(x)
+
+
+print("hi")   
+
+def main():
+    x = int(input("whats x? "))
+    if is_even(x):
+        print("even")
+    else:
+        print("odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
+
+def main():
+    x = int(input("whats x? "))
+    print("x squared is", square(x))
+
+def square(n):
+    return n*n
+
+main()
+
+#exponential power
+
+def main():
+    x = int(input("whats x? "))
+    n = int(input("whats n? "))
+    print("x to the power n is,", pow(x, n) )
+
+main()
