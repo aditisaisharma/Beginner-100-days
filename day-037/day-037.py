f1b41ce7280ea811509b52d45f68ecbbac20b034
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-09 22:46:05 +0530

    day-037

--- day-037/day-037.py
+++ day-037/day-037.py
@@ -1,2 +1,40 @@
 #Day 37 - Aditi Sai Sharma - July 9, 2026
 
+#Program 1 - Revision of Day-1
+print("hi")
+ 
+#Program 2 - Revision of Day-1
+name = input("what is your name?")
+
+#Program 3 - Revision of Day-1
+age = input("what is your age?")
+
+#Program 4 - Revision of Day-1
+print("hello, " + name)
+
+#Program 5 - different symbol used
+print("hello,", name)
+
+#Program 6 - Revision of Day-1
+for i in range(10):
+    print("life is good")
+
+#program 7 - using end 
+print("hello, ", name, end="*")
+
+#program 8 - using sep
+print("hello, ", name, sep="*")
+
+#program 8 - using sep and end both
+print("hello, ", name, end="\n", sep=" ")
+
+#program 9 - (') and (") are same in python
+print('hello, "friend"')
+
+#program 10 - using escape character
+print("hello, \"friend\"")
+
+#program 11 - f-string
+print(f"hello, {name}")
+
+print("i'm" , age , "years old", sep="~")
