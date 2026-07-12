612456ee4b746502d0b7cf6e386ef1737eb8ee92
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-12 23:22:50 +0530

    quotes

---
+++ day-040/day-040.py
@@ -0,0 +1,17 @@
+#Day 40 - Aditi Sai Sharma - July 12, 2026
+
+#quotes:
+#Create the thingd you wish existed.
+#Your future depends on what you do today.
+
+print("Create the things you wish existed.")
+print("Your future depends on what you do today.")
+print("Toleration will eventually lead to disgust.")
+print("dreams come true when you least expect it.")
+print("Feel the fear and do it anyway.")
+print("The more delusionalyy optimistic you are, the more life rewards you.")
+print("Act like the universe is obsessed with making you happy.")
+print("Once you've ruined your reputation, you can live quite freely.")
+print("I wish my eyes could take pictures.")
+print("I will attract the right people by being myself.")
+print("Don't strive to be well-known, strive to be worth knowing.")
