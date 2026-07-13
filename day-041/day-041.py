f5910ffafa6876b78c96b1ac90f5931cb0f538ed
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-13 23:22:19 +0530

    day-041

--- day-041/day-041.py
+++ day-041/day-041.py
@@ -1,1 +1,64 @@
 #Day 40 - Aditi Sai Sharma - July 12, 2026
+
+"""
+
+= means assignment in python i.e copying a thing from the right 
+to the left, as we did in input programs, whereas 
+== means equality, like in math (comparison).
+
+==   equal to
+>=
+<=
+>
+<
+!=   not equal to
+:    then
+e.g.    x<y    boolean expression"""
+
+
+x = int(input('whats x? '))
+y = int(input('whats y? '))
+
+if x>y:
+    print('x is greater than y')
+if  x<y:
+    print('x is less than y')
+if x==y:
+    print('x is equal to y')
+    
+x = int(input('whats x? '))
+y = int(input('whats y? '))
+
+if x>y:
+    print('x is greater than y')
+elif  x<y:
+    print('x is less than y')
+elif x==y:
+    print('x is equal to y')
+
+x = int(input('whats x? '))
+y = int(input('whats y? '))
+
+if x>y:
+    print('x is greater than y')
+elif  x<y:
+    print('x is less than y')
+else:
+    print('x is equal to y')
+
+x = int(input('whats x? '))
+y = int(input('whats y? '))
+
+if x>y or x<y:
+    print('x is not equal to y.')
+else:
+    print('x is equal to y.')
+
+x = int(input('whats x? '))
+y = int(input('whats y? '))
+
+if x != y:
+    print('x is not equal to y')
+else:
+    print('x is equal to y')
+
