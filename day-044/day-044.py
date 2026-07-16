da7d8f895608369dfdc1feae2749822279fb2341
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-16 23:30:59 +0530

    day-044

--- day-044/day-044.py
+++ day-044/day-044.py
@@ -1,1 +1,21 @@
 #Day 44 - Aditi Sai Sharma - July 16, 2026
+
+print("hi")
+score = int(input("score(0-100): "))
+
+
+def calcgrade(score):
+    if score >= 90 and <= 100:
+        print("A: PASS!")
+    elif score >=80:
+        print("B: PASS!")
+    elif score>=70:
+        print("C: PASS!")
+    elif score >=60:
+        print("D: PASS!")
+    elif score >=0:
+        print("F: FAIL!")
+    else:
+        ("error: invalid score.")
+
+calcgrade(score)
