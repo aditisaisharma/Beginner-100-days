0bc108fa39ece9b8685710f2f461dded5acf19a2
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-19 22:45:02 +0530

    day-047

--- day-047/day-047.py
+++ day-047/day-047.py
@@ -1,1 +1,15 @@
 #Day 47 - Aditi Sai Sharma - July 19, 2026
+
+x = int(input("whats x? "))
+
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+    
+def is_odd(x):
+    return True if x % 2 != 0 else False
+
+is_even(x)
+is_odd(x)
