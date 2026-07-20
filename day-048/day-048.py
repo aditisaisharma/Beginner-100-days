8939b04fac7d14320c31c41a7fdc89605be0a85e
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-20 23:44:39 +0530

    day-048

--- day-048/day-048.py
+++ day-048/day-048.py
@@ -1,2 +1,18 @@
 #Day 48 - Aditi Sai Sharma - July 20, 2026
 
+#match:
+
+name = input("what is your name? ")
+
+match name:
+    case "amby":
+        print("sea blue green")
+    case "lilly" | "aditi" | "karim":
+        print("brown")
+    case "carter":
+        print("brown")
+    case "ella":
+        print("blue")
+    case _:
+        print("who?")
+    
