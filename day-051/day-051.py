2db43aba8964f9f69b5569122b93f52cdb251964
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-23 23:32:10 +0530

    day-051

--- day-051/day-051.py
+++ day-051/day-051.py
@@ -1,1 +1,25 @@
 #Day 51 - Aditi Sai Sharma - July 23, 2026
+
+
+#Program 1 - greetings
+print("hola amigo")
+
+#Program 2 -
+name = input("what is your name? ")
+print("hello, ", name)
+
+#Program 3 - Remove white spaces from str
+name = name.strip()
+
+#Program 4 - Capitalize first letter
+name = name.capitalize()
+
+#Programc 5 - Capitalize all 1st letters
+name = name.title()
+
+#Program 6 - altogether
+place = input("where do you live? ").strip().title()
+
+#Program 7 - split first, middle, last names
+first, middle, last = name.split(" ")
+print("hello, ", middle)
