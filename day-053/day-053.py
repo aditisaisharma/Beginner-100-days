03a8adb32ede221337c330a026e200a60dccfdb3
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-25 23:23:07 +0530

    day-053

--- day-053/day-053.py
+++ day-053/day-053.py
@@ -18,3 +18,8 @@ print("jhukti hai duniya, bas jhukane wala chahiye.")
 print('crazypoweringyouthofindia')
 
 print("long live revolution, history created.")
+
+print("Education minister resigns, paving the path to an educated India.")
+print("nice things are about to happen, great things are bound to happen")
+print("we did it. our country will be recognised as sone ki chidiya once again, very very soon.")
+print("This is the Beginning. greatness shall prevail.")
