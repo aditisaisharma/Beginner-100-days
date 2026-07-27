33319d8c686b40e044a9ef23f40ff7c859b48d10
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-28 00:14:47 +0530

    day-055

--- day-055/day-055.py
+++ day-055/day-055.py
@@ -1,2 +1,24 @@
 #Day 55 - Aditi Sai Sharma - July 27, 2026
 
+#defining functions
+def hello():
+    print("hello")
+
+name = input("whats your name?" ) 
+hello()
+print(name)
+
+#2 hello, to - name same line
+def hello(to):
+    print("hello,", to)
+
+name = input("whats your name? ")
+hello(name)
+
+#3 default value
+def hello(to="world"):
+    print("hello,", to)
+
+hello()
+name = input("what is your name? ")
+hello(name)
