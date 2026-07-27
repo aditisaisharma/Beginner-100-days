121ebb50a395fe55549a0dc53b1f97c7b2ca8c29
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-28 00:19:25 +0530

    day-056

--- day-056/day-056.py
+++ day-056/day-056.py
@@ -1,1 +1,20 @@
 #Day 56 - Aditi Sai Sharma - July 28, 2026
+#boolean value - True/False (capital T and capital F)
+print("hi")   
+
+def main():
+    x = int(input("whats x? "))
+    if is_even(x):
+        print("even")
+    else:
+        print("odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
+
+
