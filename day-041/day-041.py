a26a9d6ded043aaeaed7c10e481f3e0e5bda7d69
Aditi Sai Sharma <aditisaisharmaofficial@gmail.com>
2026-07-30 23:47:03 +0530

    day-058

--- day-041/day-041.py
+++ day-041/day-041.py
@@ -1,30 +1,4 @@
 #Day 41 - Aditi Sai Sharma - July 13, 2026
-
-"""
-
-= means assignment in python i.e copying a thing from the right 
-to the left, as we did in input programs, whereas 
-== means equality, like in math (comparison).
-
-==   equal to
->=
-<=
->
-<
-!=   not equal to
-:    then
-e.g.    x<y    boolean expression"""
-
-
-x = int(input('whats x? '))
-y = int(input('whats y? '))
-
-if x>y:
-    print('x is greater than y')
-if  x<y:
-    print('x is less than y')
-if x==y:
-    print('x is equal to y')
     
 x = int(input('whats x? '))
 y = int(input('whats y? '))
@@ -63,3 +37,28 @@ else:
     print('x is equal to y')
 
 
+"""
+
+= means assignment in python i.e copying a thing from the right 
+to the left, as we did in input programs, whereas 
+== means equality, like in math (comparison).
+
+==   equal to
+>=
+<=
+>
+<
+!=   not equal to
+:    then
+e.g.    x<y    boolean expression"""
+
+
+x = int(input('whats x? '))
+y = int(input('whats y? '))
+
+if x>y:
+        print('x is greater than y')
+    if  x<y:
+        print('x is less than y')
+    if x==y:
+        print('x is equal to y')
---
+++ day-058/day-058.py
@@ -0,0 +1,27 @@
+#Day 58 - Aditi Sai Sharma - July 30, 2026
+
+"""
+
+= means assignment in python i.e copying a thing from the right 
+to the left, as we did in input programs, whereas 
+== means equality, like in math (comparison).
+
+==   equal to
+>=
+<=
+>
+<
+!=   not equal to
+:    then
+e.g.    x<y    boolean expression"""
+
+
+x = int(input('whats x? '))
+y = int(input('whats y? '))
+
+if x>y:
+     print('x is greater than y')
+if  x<y:
+        print('x is less than y')
+if x==y:
+        print('x is equal to y')
